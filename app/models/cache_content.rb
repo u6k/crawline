0bba023a11d72c77cda3c9533c0b2070dddf53ba
u6k <u6k.apps@gmail.com>
2018-10-15 17:30:25 +0900

    modify CacheContent model
    - hash style ... symbol -> string
    - s3 data compress 7zip

--- app/models/cache_content.rb
+++ app/models/cache_content.rb
@@ -4,12 +4,12 @@ class CacheContent < ApplicationRecord
 
   def self.new_from_request_and_response(request, response, content, content_version = nil)
     # check
-    raise "Invalid. request.url is empty." if request[:url].blank?
-    raise "Invalid. request.method is empty." if request[:method].blank?
-    raise "Invalid. request.method is not GET or POST. (method=#{request[:method]})" if request[:method] != "GET" && downloaded_content[:method] != "POST"
-    raise "Invalid. request.headers is not Hash." if request[:parameters].present? && (not request[:parameters].instance_of?(Hash))
-    raise "Invalid. response.headers is empty." if response[:headers].blank?
-    raise "Invalid. content is empty." if content.blank?
+    raise "Invalid. request.url is blank." if request["url"].blank?
+    raise "Invalid. request.method is blank." if request["method"].blank?
+    raise "Invalid. request.method is not GET or POST. (method=#{request["method"]})" if request["method"] != "GET" && request["method"] != "POST"
+    raise "Invalid. request.headers is not Hash." if request["parameters"].present? && (not request["parameters"].instance_of?(Hash))
+    raise "Invalid. response.headers is blank." if response["headers"].blank?
+    raise "Invalid. content is nil." if content.nil?
     raise "Invalid. content_version is not integer. (content_version=#{content_version})" if content_version.present? && (not content_version.integer?)
 
     # build upload content
@@ -18,19 +18,18 @@ class CacheContent < ApplicationRecord
       content_version = Time.zone.now.utc.to_i
     end
 
-    content_data = {
-      request: request,
-      response: response,
-      cached_timestamp: content_version,
-      content: "Base64:" + Base64.strict_encode64(content),
-      content_hash: "SHA256:" + OpenSSL::Digest::SHA256.hexdigest(content)
+    content_meta = {
+      "request" => request,
+      "response" => response,
+      "cached_timestamp" => content_version,
     }
 
     # content 7zip compress
     content_data_7z = StringIO.new("")
     SevenZipRuby::Writer.open(content_data_7z) do |szr|
       szr.level = 9
-      szr.add_data(content_data.to_json, cache_hash + ".7z")
+      szr.add_data(content_meta.to_json, cache_hash + ".meta")
+      szr.add_data(content, cache_hash + ".bin")
     end
 
     content_data_7z.rewind
@@ -48,46 +47,43 @@ class CacheContent < ApplicationRecord
 
   def self.find_by_request(request)
     # check
-    if request[:url].empty?
-      raise "Invalid. url is empty."
-    end
-    if request[:method].empty?
-      raise "Invalid. method is empty."
-    end
-    if request[:method] != "GET" && request[:method] != "POST"
-      raise "Invalid. method is not GET or POST. (method=#{request[:method]})"
-    end
+    raise "Invalid. url is blank." if request["url"].blank?
+    raise "Invalid. method is blank." if request["method"].blank?
+    raise "Invalid. method is not GET or POST. (method=#{request["method"]})" if request["method"] != "GET" && request["method"] != "POST"
+    raise "Invalid. request.headers is not Hash." if request["parameters"].present? && (not request["parameters"].instance_of?(Hash))
 
     # find CacheContent
-    cache_hash = OpenSSL::Digest::SHA256.hexdigest(content.to_json)
+    cache_hash = OpenSSL::Digest::SHA256.hexdigest(request.to_json)
 
-    cache = CacheContent.where(cache_hash: cache_hash).order("downloaded_timestamp desc").first
+    cache = CacheContent.where(cache_hash: cache_hash).order(content_version: :desc).first
   end
 
   def to_cache
     # download content_data, and decompress
-    content_data = self.content.download
+    content_data_7z = self.content.download
 
-    SevenZipRuby::Reader.open(content_data) do |szr|
-      content = szr.extract_data(szr.entries[0])
+    content_meta = nil
+    content = nil
+    SevenZipRuby::Reader.open(StringIO.new(content_data_7z)) do |szr|
+      szr.entries.each do |entry|
+        pp entry.path
+        if entry.path.end_with?(".meta")
+          content_meta = JSON.parse(szr.extract_data(entry))
+        elsif entry.path.end_with?(".bin")
+          content = szr.extract_data(entry)
+        end
+      end
     end
+
+    raise "Invalid. meta or bin not found." if content_meta.nil? || content.nil?
 
     # build cache
     cache = {
-      url: content[:url],
-      method: content[:method]
+      "request" => content_meta["request"],
+      "response" => content_meta["response"],
+      "cached_timestamp" => content_meta["cached_timestamp"],
+      "content" => content
     }
-    if not content[:request_parameters].empty?
-      cache[:request_parameters] = content[:request_parameters]
-    end
-    cache[:response_headers] = content[:response_headers]
-    cache[:downloaded_timestamp] = content[:downloaded_timestamp]
-
-    raise "Invalid. content not start with 'Base64:'." if not content[:content].start_with?("Base64:")
-
-    cache[:content] = Base64.strict_decode64(content[:content][6, -1])
-
-    raise "Invalid. content hash not match." if content[:content_hash] != OpenSSL::Digest::SHA256.hexdigest(cache[:content])
 
     # return
     cache
--- test/models/cache_content_test.rb
+++ test/models/cache_content_test.rb
@@ -2,18 +2,178 @@ require 'test_helper'
 
 class CacheContentTest < ActiveSupport::TestCase
 
-  test "save cache: 1 cache" do
-    # setup
+  def setup
+    # save blog_page
     request = {
-      url: "https://blog.u6k.me/001.html",
-      method: "GET"
+      "url" => "https://blog.u6k.me/2018/06/09/my-development-environment-and-deploy-workflow.html",
+      "method" => "GET"
     }
 
     response = {
-      headers: {
-        "server": "nginx",
-        "content-type": "text/html; charset=utf-8",
-        "date": "Tue, 09 Oct 2018 02:51:57 GMT"
+      "headers" => {
+        "server" => "nginx",
+        "content-type" => "text/plain"
+      }
+    }
+
+    content = "blog_page_1"
+
+    cache_content = CacheContent.new_from_request_and_response(request, response, content, 1)
+    cache_content.save!
+
+    content = "blog_page_2"
+
+    cache_content = CacheContent.new_from_request_and_response(request, response, content, 2)
+    cache_content.save!
+
+    response = {
+      "headers" => {
+        "server" => "GitHub.com",
+        "content-type" => "text/html; charset=utf-8",
+        "last-modified" => "Fri, 13 Jul 2018 17:08:50 GMT",
+        "etag" => "5b48dca2-58fd",
+        "access-control-allow-origin" => "*",
+        "expires" => "Tue, 09 Oct 2018 03:01:56 GMT",
+        "cache-control" => "max-age=600",
+        "x-github-request-id" => "A482:358A:102F627:160CADB:5BBC17CC",
+        "accept-ranges" => "bytes",
+        "date" => "Tue, 09 Oct 2018 02:51:57 GMT",
+        "via" => "1.1 varnish",
+        "age" => "0",
+        "x-served-by" => "cache-sin18022-SIN",
+        "x-cache" => "MISS",
+        "x-cache-hits" => "0",
+        "x-timer" => "S1539053517.804919,VS0,VE237",
+        "vary" => "Accept-Encoding",
+        "x-fastly-request-id" => "0f079439d28fe3ae5e03de983785f513eb668167",
+        "content-length" => "22781"
+      }
+    }
+
+    content = File.open("test/fixtures/files/blog_page.html").read
+    content_version = Time.zone.local(2018, 10, 9, 11, 1, 56).to_i
+
+    cache_content = CacheContent.new_from_request_and_response(request, response, content, content_version)
+    cache_content.save!
+
+    # save blog tags
+    request = {
+      "url" => "https://kabuoji3.com/tags/",
+      "method" => "POST",
+      "parameters" => {
+        "tag" => "Docker"
+      }
+    }
+
+    response = {
+      "headers" => {
+        "server" => "nginx",
+        "content-type" => "text/plain"
+      }
+    }
+
+    content = "blog_tags_1"
+
+    cache_content = CacheContent.new_from_request_and_response(request, response, content, 1)
+    cache_content.save!
+
+    content = "blog_tags_2"
+
+    cache_content = CacheContent.new_from_request_and_response(request, response, content, 2)
+    cache_content.save!
+
+    response = {
+      "headers" => {
+        "server" => "GitHub.com",
+        "content-type" => "text/html; charset=utf-8",
+        "last-modified" => "Fri, 13 Jul 2018 17:08:50 GMT",
+        "etag" => "5b48dca2-3c1a9",
+        "access-control-allow-origin" => "*",
+        "expires" => "Tue, 09 Oct 2018 03:13:51 GMT",
+        "cache-control" => "max-age=600",
+        "x-github-request-id" => "D330:06AC:5AEE2DB:75C2949:5BBC1A97",
+        "accept-ranges" => "bytes",
+        "date" => "Tue, 09 Oct 2018 03:03:51 GMT",
+        "via" => "1.1 varnish",
+        "age" => "0",
+        "x-served-by" => "cache-sin18024-SIN",
+        "x-cache" => "MISS",
+        "x-cache-hits" => "0",
+        "x-timer" => "S1539054232.662714,VS0,VE244",
+        "vary" => "Accept-Encoding",
+        "x-fastly-request-id" => "fd4a9481023f92db718dfa8a1986786bb792bb60",
+        "content-length" => "246185"
+      }
+    }
+
+    content = File.open("test/fixtures/files/blog_tags.html").read
+    content_version = Time.zone.local(2018, 10, 9, 11, 3, 51).to_i
+
+    cache_content = CacheContent.new_from_request_and_response(request, response, content, content_version)
+    cache_content.save!
+
+    # save binary
+    request = {
+      "url" => "http://gravatar.com/avatar/7cbe65037b17a6ee6711ce18b1e97638",
+      "method" => "GET"
+    }
+
+    response = {
+      "headers" => {
+        "server" => "nginx",
+        "content-type" => "text/plain"
+      }
+    }
+
+    content = "binary_1"
+
+    cache_content = CacheContent.new_from_request_and_response(request, response, content, 1)
+    cache_content.save!
+
+    content = "binary_2"
+
+    cache_content = CacheContent.new_from_request_and_response(request, response, content, 2)
+    cache_content.save!
+
+    response = {
+      "headers" => {
+        "Server" => "nginx",
+        "Date" => "Tue, 09 Oct 2018 03:15:49 GMT",
+        "Content-Type" => "image/png",
+        "Content-Length" => "10012",
+        "Connection" => "keep-alive",
+        "Last-Modified" => "Thu, 29 Dec 2016 10:21:55 GMT",
+        "Link" => "<http://www.gravatar.com/avatar/7cbe65037b17a6ee6711ce18b1e97638>; rel=\"canonical\"",
+        "Content-Disposition" => "inline; filename=\"7cbe65037b17a6ee6711ce18b1e97638.png\"",
+        "Access-Control-Allow-Origin" => "*",
+        "X-Varnish" => "1030785095 825725510",
+        "Via" => "1.1 varnish-v4",
+        "Accept-Ranges" => "bytes",
+        "Expires" => "Tue, 09 Oct 2018 03:20:49 GMT",
+        "Cache-Control" => "max-age=300",
+        "Source-Age" => "4252"
+      }
+    }
+
+    content = File.open("test/fixtures/files/gravatar.png").read
+    content_version = Time.zone.local(2018, 10, 9, 11, 3, 15).to_i
+
+    cache_content = CacheContent.new_from_request_and_response(request, response, content, content_version)
+    cache_content.save!
+  end
+
+  test "save cache: 1 cache" do
+    # setup
+    request = {
+      "url" => "https://blog.u6k.me/001.html",
+      "method" => "GET"
+    }
+
+    response = {
+      "headers" => {
+        "server" => "nginx",
+        "content-type" => "text/html; charset=utf-8",
+        "date" => "Tue, 09 Oct 2018 02:51:57 GMT"
       }
     }
 
@@ -38,158 +198,206 @@ class CacheContentTest < ActiveSupport::TestCase
       content_data = szr.extract_data(szr.entries[0])
     end
     expected_content_data = {
-      request: request,
-      response: response,
-      cached_timestamp: 123,
-      content: "Base64:" + Base64.strict_encode64(content),
-      content_hash: "SHA256:" + OpenSSL::Digest::SHA256.hexdigest(content)
+      "request" => request,
+      "response" => response,
+      "cached_timestamp" => 123,
+      "content" => "Base64:" + Base64.strict_encode64(content),
+      "content_hash" => "SHA256:" + OpenSSL::Digest::SHA256.hexdigest(content)
     }.to_json
 
     assert_equal expected_content_data, content_data
   end
 
-  test "find cache: not found" do
+  test "save cache: cache_version not setting(default)" do
+    # setup
+    started_timestamp = Time.zone.now.utc.to_i
+
     request = {
-      url: "https://blog.u6k.me/foo/bar/boo.html",
-      method: "GET"
+      "url" => "https://blog.u6k.me/001.html",
+      "method" => "GET"
     }
 
+    response = {
+      "headers" => {
+        "server" => "nginx",
+        "content-type" => "text/html; charset=utf-8",
+        "date" => "Tue, 09 Oct 2018 02:51:57 GMT"
+      }
+    }
+
+    content = File.open("test/fixtures/files/blog_page.html").read
+
+    # execute
+    cache_content = CacheContent.new_from_request_and_response(request, response, content)
+    cache_content.save!
+
+    # check
+    cache_contents = CacheContent.where(cache_hash: "1917509773f0d090635b1b513627c6e4774acf4b093934255004e77320ea1270")
+
+    assert_equal 1, cache_contents.length
+
+    cache_content = cache_contents[0]
+
+    assert started_timestamp <= cache_content.content_version
+  end
+
+  test "find cache: not found" do
+    # setup
+    request = {
+      "url" => "https://blog.u6k.me/foo/bar/boo.html",
+      "method" => "GET"
+    }
+
+    # execute
     cache_content = CacheContent.find_by_request(request)
 
+    # check
     assert_nil cache_content
   end
 
   test "find cache: get request" do
+    # setup
     request = {
-      url: "https://blog.u6k.me/2018/06/09/my-development-environment-and-deploy-workflow.html",
-      method: "GET"
+      "url" => "https://blog.u6k.me/2018/06/09/my-development-environment-and-deploy-workflow.html",
+      "method" => "GET"
     }
 
+    # execute
     cache_content = CacheContent.find_by_request(request)
+
+    # check
+    expected_cache = {
+      "request" => {
+        "url" => "https://blog.u6k.me/2018/06/09/my-development-environment-and-deploy-workflow.html",
+        "method" => "GET"
+      },
+      "response" => {
+        "headers" => {
+          "server" => "GitHub.com",
+          "content-type" => "text/html; charset=utf-8",
+          "last-modified" => "Fri, 13 Jul 2018 17:08:50 GMT",
+          "etag" => "5b48dca2-58fd",
+          "access-control-allow-origin" => "*",
+          "expires" => "Tue, 09 Oct 2018 03:01:56 GMT",
+          "cache-control" => "max-age=600",
+          "x-github-request-id" => "A482:358A:102F627:160CADB:5BBC17CC",
+          "accept-ranges" => "bytes",
+          "date" => "Tue, 09 Oct 2018 02:51:57 GMT",
+          "via" => "1.1 varnish",
+          "age" => "0",
+          "x-served-by" => "cache-sin18022-SIN",
+          "x-cache" => "MISS",
+          "x-cache-hits" => "0",
+          "x-timer" => "S1539053517.804919,VS0,VE237",
+          "vary" => "Accept-Encoding",
+          "x-fastly-request-id" => "0f079439d28fe3ae5e03de983785f513eb668167",
+          "content-length" => "22781"
+        }
+      },
+      "cached_timestamp" => Time.zone.local(2018, 10, 9, 11, 1, 56).to_i,
+      "content" => File.open("test/fixtures/files/blog_page.html").read
+    }
 
     assert_equal "6b44e4173ba3995850ac8d5a251fc7a6a8184925c482bbc6053ab66f99f6d2ef", cache_content.cache_hash
     assert_equal 1539082916, cache_content.content_version
-    expected_cache = {
-      request: {
-        url: "https://blog.u6k.me/2018/06/09/my-development-environment-and-deploy-workflow.html",
-        method: "GET"
-      },
-      response: {
-        headers: {
-          "server": "GitHub.com",
-          "content-type": "text/html; charset=utf-8",
-          "last-modified": "Fri, 13 Jul 2018 17:08:50 GMT",
-          "etag": "5b48dca2-58fd",
-          "access-control-allow-origin": "*",
-          "expires": "Tue, 09 Oct 2018 03:01:56 GMT",
-          "cache-control": "max-age=600",
-          "x-github-request-id": "A482:358A:102F627:160CADB:5BBC17CC",
-          "accept-ranges": "bytes",
-          "date": "Tue, 09 Oct 2018 02:51:57 GMT",
-          "via": "1.1 varnish",
-          "age": "0",
-          "x-served-by": "cache-sin18022-SIN",
-          "x-cache": "MISS",
-          "x-cache-hits": "0",
-          "x-timer": "S1539053517.804919,VS0,VE237",
-          "vary": "Accept-Encoding",
-          "x-fastly-request-id": "0f079439d28fe3ae5e03de983785f513eb668167",
-          "content-length": "22781"
-        }
-      },
-      cached_timestamp: Time.zone.local(2018, 10, 9, 11, 1, 56),
-      content: File.open("test/fixtures/files/blog_page.html").read
-    }
     assert_equal expected_cache, cache_content.to_cache
   end
 
   test "find cache: post request" do
+    # setup
     request = {
-      url: "https://kabuoji3.com/tags/",
-      method: "POST",
-      parameters: {
-        "tag": "Docker"
+      "url" => "https://kabuoji3.com/tags/",
+      "method" => "POST",
+      "parameters" => {
+        "tag" => "Docker"
       }
     }
 
+    # execute
     cache_content = CacheContent.find_by_request(request)
+
+    # check
+    expected_cache = {
+      "request" => {
+        "url" => "https://kabuoji3.com/tags/",
+        "method" => "POST",
+        "parameters" => {
+          "tag" => "Docker"
+        }
+      },
+      "response" => {
+        "headers" => {
+          "server" => "GitHub.com",
+          "content-type" => "text/html; charset=utf-8",
+          "last-modified" => "Fri, 13 Jul 2018 17:08:50 GMT",
+          "etag" => "5b48dca2-3c1a9",
+          "access-control-allow-origin" => "*",
+          "expires" => "Tue, 09 Oct 2018 03:13:51 GMT",
+          "cache-control" => "max-age=600",
+          "x-github-request-id" => "D330:06AC:5AEE2DB:75C2949:5BBC1A97",
+          "accept-ranges" => "bytes",
+          "date" => "Tue, 09 Oct 2018 03:03:51 GMT",
+          "via" => "1.1 varnish",
+          "age" => "0",
+          "x-served-by" => "cache-sin18024-SIN",
+          "x-cache" => "MISS",
+          "x-cache-hits" => "0",
+          "x-timer" => "S1539054232.662714,VS0,VE244",
+          "vary" => "Accept-Encoding",
+          "x-fastly-request-id" => "fd4a9481023f92db718dfa8a1986786bb792bb60",
+          "content-length" => "246185"
+        }
+      },
+      "cached_timestamp" => Time.zone.local(2018, 10, 9, 11, 3, 51).to_i,
+      "content" => File.open("test/fixtures/files/blog_tags.html").read
+    }
 
     assert_equal "409c87b61b9941c0f142f5ca277cb94a7b3dbad54372bfe9847bd54e70b85228", cache_content.cache_hash
     assert_equal 1539083031, cache_content.content_version
-    expected_cache = {
-      request: {
-        url: "https://kabuoji3.com/tags/",
-        method: "POST",
-        parameters: {
-          "tag": "Docker"
-        }
-      },
-      response: {
-        headers: {
-          "server": "GitHub.com",
-          "content-type": "text/html; charset=utf-8",
-          "last-modified": "Fri, 13 Jul 2018 17:08:50 GMT",
-          "etag": "5b48dca2-3c1a9",
-          "access-control-allow-origin": "*",
-          "expires": "Tue, 09 Oct 2018 03:13:51 GMT",
-          "cache-control": "max-age=600",
-          "x-github-request-id": "D330:06AC:5AEE2DB:75C2949:5BBC1A97",
-          "accept-ranges": "bytes",
-          "date": "Tue, 09 Oct 2018 03:03:51 GMT",
-          "via": "1.1 varnish",
-          "age": "0",
-          "x-served-by": "cache-sin18024-SIN",
-          "x-cache": "MISS",
-          "x-cache-hits": "0",
-          "x-timer": "S1539054232.662714,VS0,VE244",
-          "vary": "Accept-Encoding",
-          "x-fastly-request-id": "fd4a9481023f92db718dfa8a1986786bb792bb60",
-          "content-length": "246185"
-        }
-      },
-      cached_timestamp: Time.zone.local(2018, 10, 9, 11, 3, 51),
-      content: File.open("test/fixtures/files/blog_tags.html").read
-    }
     assert_equal expected_cache, cache_content.to_cache
   end
 
   test "find cache: binary content" do
+    # setup
     request = {
-      url: "http://gravatar.com/avatar/7cbe65037b17a6ee6711ce18b1e97638",
-      method: "GET"
+      "url" => "http://gravatar.com/avatar/7cbe65037b17a6ee6711ce18b1e97638",
+      "method" => "GET"
     }
 
+    # execute
     cache_content = CacheContent.find_by_request(request)
+
+    # check
+    expected_cache = {
+      "request" => {
+        "url" => "http://gravatar.com/avatar/7cbe65037b17a6ee6711ce18b1e97638",
+        "method" => "GET"
+      },
+      "response" => {
+        "headers" => {
+          "Server" => "nginx",
+          "Date" => "Tue, 09 Oct 2018 03:15:49 GMT",
+          "Content-Type" => "image/png",
+          "Content-Length" => "10012",
+          "Connection" => "keep-alive",
+          "Last-Modified" => "Thu, 29 Dec 2016 10:21:55 GMT",
+          "Link" => "<http://www.gravatar.com/avatar/7cbe65037b17a6ee6711ce18b1e97638>; rel=\"canonical\"",
+          "Content-Disposition" => "inline; filename=\"7cbe65037b17a6ee6711ce18b1e97638.png\"",
+          "Access-Control-Allow-Origin" => "*",
+          "X-Varnish" => "1030785095 825725510",
+          "Via" => "1.1 varnish-v4",
+          "Accept-Ranges" => "bytes",
+          "Expires" => "Tue, 09 Oct 2018 03:20:49 GMT",
+          "Cache-Control" => "max-age=300",
+          "Source-Age" => "4252"
+        }
+      },
+      "cached_timestamp" => Time.zone.local(2018, 10, 9, 11, 3, 15).to_i,
+      "content" => File.open("test/fixtures/files/gravatar.png").read
+    }
 
     assert_equal "5a6041fa4c3379560cb0cb4b0cee8f6be3adf3db7396ef8b21a557c8f37e4456", cache_content.cache_hash
     assert_equal 1539082995, cache_content.content_version
-    expected_cache = {
-      request: {
-        url: "http://gravatar.com/avatar/7cbe65037b17a6ee6711ce18b1e97638",
-        method: "GET"
-      },
-      response: {
-        headers: {
-          "Server": "nginx",
-          "Date": "Tue, 09 Oct 2018 03:15:49 GMT",
-          "Content-Type": "image/png",
-          "Content-Length": "10012",
-          "Connection": "keep-alive",
-          "Last-Modified": "Thu, 29 Dec 2016 10:21:55 GMT",
-          "Link": "<http://www.gravatar.com/avatar/7cbe65037b17a6ee6711ce18b1e97638>; rel=\"canonical\"",
-          "Content-Disposition": "inline; filename=\"7cbe65037b17a6ee6711ce18b1e97638.png\"",
-          "Access-Control-Allow-Origin": "*",
-          "X-Varnish": "1030785095 825725510",
-          "Via": "1.1 varnish-v4",
-          "Accept-Ranges": "bytes",
-          "Expires": "Tue, 09 Oct 2018 03:20:49 GMT",
-          "Cache-Control": "max-age=300",
-          "Source-Age": "4252"
-        }
-      },
-      cached_timestamp: Time.zone.local(2018, 10, 9, 11, 3, 15),
-      content: File.open("test/fixtures/files/gravatar.png").read
-    }
     assert_equal expected_cache, cache_content.to_cache
   end
 
