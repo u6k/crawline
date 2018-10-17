ba2589aad12789848c1777ce780955edbd481680
u6k <u6k.apps@gmail.com>
2018-10-17 17:24:00 +0900

    impl download with post, and test.

--- app/models/concerns/scraper.rb
+++ app/models/concerns/scraper.rb
@@ -1,5 +1,7 @@
 class Scraper
   extend ActiveSupport::Concern
+
+  attr_accessor :download_interval
   
   @@rules = []
   
@@ -7,12 +9,22 @@ class Scraper
     @@rules << rule
   end
 
+  def initialize()
+    @download_interval = 1
+  end
+
   def find_rule(request)
     rule = @@rules.find { |rule| rule.match_request?(request) }
   end
 
   def download(request)
-    download_with_get(request["url"])
+    if request["method"] == "GET"
+      download_with_get(request["url"])
+    elsif request["method"] == "POST"
+      download_with_post(request["url"], request["parameters"])
+    else
+      Rails.logger.warn "download: method is not GET or POST. method=#{request["method"]}"
+    end
   end
 
   private
@@ -28,7 +40,7 @@ class Scraper
       http.request(req)
     end
 
-    sleep(1) # FIXME
+    sleep(@download_interval) # FIXME
 
     if res.code == "200"
       response = {
@@ -43,6 +55,37 @@ class Scraper
     else
       # FIXME
       Rails.logger.warn "download_with_get: status code not 200 ok: url=#{url}, code=#{res.code}"
+      nil
+    end
+  end
+
+  def download_with_post(url, data)
+    uri = URI(url)
+
+    req = Net::HTTP::Post.new(uri)
+    req["User-Agent"] = "curl/7.54.0" # FIXME
+    req["Accept"] = "*/*" # FIXME
+    req.set_form_data(data)
+
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == "https") do |http|
+      http.request(req)
+    end
+
+    sleep(@download_interval) # FIXME
+
+    if res.code == "200"
+      response = {
+        "headers" => {},
+        "content" => res.body
+      }
+      res.each do |name, val|
+        response["headers"][name] = val
+      end
+
+      response
+    else
+      # FIXME
+      Rails.logger.warn "download_with_post: status code not 200 ok: url=#{url}, data=#{data}, code=#{res.code}"
       nil
     end
   end
--- test/models/concerns/scraper_download_test.rb
+++ test/models/concerns/scraper_download_test.rb
@@ -4,11 +4,62 @@ require "webmock/minitest"
 class ScraperDownloadTest < ActiveSupport::TestCase
 
   def setup
+    # stub request
     stub_request(:get, "https://blog.u6k.me/2018/06/09/my-development-environment-and-deploy-workflow.html").to_return(
       status: 200,
-      headers: { "server" => "nginx", "content-type" => "text/plain" },
+      headers: {
+        "server" => "GitHub.com",
+        "content-type" => "text/html; charset=utf-8",
+        "last-modified" => "Fri, 13 Jul 2018 17:08:50 GMT",
+        "etag" => "5b48dca2-3c1a9",
+        "access-control-allow-origin" => "*",
+        "expires" => "Tue, 09 Oct 2018 03:13:51 GMT",
+        "cache-control" => "max-age=600",
+        "x-github-request-id" => "D330:06AC:5AEE2DB:75C2949:5BBC1A97",
+        "accept-ranges" => "bytes",
+        "date" => "Tue, 09 Oct 2018 03:03:51 GMT",
+        "via" => "1.1 varnish",
+        "age" => "0",
+        "x-served-by" => "cache-sin18024-SIN",
+        "x-cache" => "MISS",
+        "x-cache-hits" => "0",
+        "x-timer" => "S1539054232.662714,VS0,VE244",
+        "vary" => "Accept-Encoding",
+        "x-fastly-request-id" => "fd4a9481023f92db718dfa8a1986786bb792bb60",
+        "content-length" => "246185"
+      },
       body: File.open("test/fixtures/files/blog_page.html").read
     )
+
+    stub_request(:post, "https://blog.u6k.me/tags/").with(body: { "tag": "Docker" }).to_return(
+      status: 200,
+      headers: {
+        "server" => "GitHub.com",
+        "content-type" => "text/html; charset=utf-8",
+        "last-modified" => "Fri, 13 Jul 2018 17:08:50 GMT",
+        "etag" => "5b48dca2-3c1a9",
+        "access-control-allow-origin" => "*",
+        "expires" => "Tue, 09 Oct 2018 03:13:51 GMT",
+        "cache-control" => "max-age=600",
+        "x-github-request-id" => "D330:06AC:5AEE2DB:75C2949:5BBC1A97",
+        "accept-ranges" => "bytes",
+        "date" => "Tue, 09 Oct 2018 03:03:51 GMT",
+        "via" => "1.1 varnish",
+        "age" => "0",
+        "x-served-by" => "cache-sin18024-SIN",
+        "x-cache" => "MISS",
+        "x-cache-hits" => "0",
+        "x-timer" => "S1539054232.662714,VS0,VE244",
+        "vary" => "Accept-Encoding",
+        "x-fastly-request-id" => "fd4a9481023f92db718dfa8a1986786bb792bb60",
+        "content-length" => "246185"
+      },
+      body: File.open("test/fixtures/files/blog_tags.html").read
+    )
+
+    # build Scraper
+    @scraper = Scraper.new
+    @scraper.download_interval = 0.001
   end
 
   test "download: get request" do
@@ -19,14 +70,30 @@ class ScraperDownloadTest < ActiveSupport::TestCase
     }
 
     # execute
-    scraper = Scraper.new
-    response = scraper.download(request)
+    response = @scraper.download(request)
 
     # check
     expected_response = {
       "headers" => {
-        "server" => "nginx",
-        "content-type" => "text/plain"
+        "server" => "GitHub.com",
+        "content-type" => "text/html; charset=utf-8",
+        "last-modified" => "Fri, 13 Jul 2018 17:08:50 GMT",
+        "etag" => "5b48dca2-3c1a9",
+        "access-control-allow-origin" => "*",
+        "expires" => "Tue, 09 Oct 2018 03:13:51 GMT",
+        "cache-control" => "max-age=600",
+        "x-github-request-id" => "D330:06AC:5AEE2DB:75C2949:5BBC1A97",
+        "accept-ranges" => "bytes",
+        "date" => "Tue, 09 Oct 2018 03:03:51 GMT",
+        "via" => "1.1 varnish",
+        "age" => "0",
+        "x-served-by" => "cache-sin18024-SIN",
+        "x-cache" => "MISS",
+        "x-cache-hits" => "0",
+        "x-timer" => "S1539054232.662714,VS0,VE244",
+        "vary" => "Accept-Encoding",
+        "x-fastly-request-id" => "fd4a9481023f92db718dfa8a1986786bb792bb60",
+        "content-length" => "246185"
       },
       "content" => File.open("test/fixtures/files/blog_page.html").read
     }
@@ -35,26 +102,84 @@ class ScraperDownloadTest < ActiveSupport::TestCase
   end
 
   test "download: post request" do
+    # setup
+    request = {
+      "url" => "https://blog.u6k.me/tags/",
+      "method" => "POST",
+      "parameters" => {
+        "tag" => "Docker"
+      }
+    }
+
+    # execute
+    response = @scraper.download(request)
+
+    # check
+    expected_response = {
+      "headers" => {
+        "server" => "GitHub.com",
+        "content-type" => "text/html; charset=utf-8",
+        "last-modified" => "Fri, 13 Jul 2018 17:08:50 GMT",
+        "etag" => "5b48dca2-3c1a9",
+        "access-control-allow-origin" => "*",
+        "expires" => "Tue, 09 Oct 2018 03:13:51 GMT",
+        "cache-control" => "max-age=600",
+        "x-github-request-id" => "D330:06AC:5AEE2DB:75C2949:5BBC1A97",
+        "accept-ranges" => "bytes",
+        "date" => "Tue, 09 Oct 2018 03:03:51 GMT",
+        "via" => "1.1 varnish",
+        "age" => "0",
+        "x-served-by" => "cache-sin18024-SIN",
+        "x-cache" => "MISS",
+        "x-cache-hits" => "0",
+        "x-timer" => "S1539054232.662714,VS0,VE244",
+        "vary" => "Accept-Encoding",
+        "x-fastly-request-id" => "fd4a9481023f92db718dfa8a1986786bb792bb60",
+        "content-length" => "246185"
+      },
+      "content" => File.open("test/fixtures/files/blog_tags.html").read
+    }
+
+    assert_equal response, expected_response
+  end
+
+  test "download: get redirect" do
     flunk
   end
 
-  test "download: redirect" do
+  test "download: post redirect" do
     flunk
   end
 
-  test "download: not modified" do
+  test "download: get not modified" do
     flunk
   end
 
-  test "download: not found" do
+  test "download: post not modified" do
     flunk
   end
 
-  test "download: internal server error" do
+  test "download: get not found" do
     flunk
   end
 
-  test "download: timeout" do
+  test "download: post not found" do
+    flunk
+  end
+
+  test "download: get internal server error" do
+    flunk
+  end
+
+  test "download: post internal server error" do
+    flunk
+  end
+
+  test "download: get timeout" do
+    flunk
+  end
+
+  test "download: post timeout" do
     flunk
   end
 
