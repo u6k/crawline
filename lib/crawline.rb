f13121e058b6d8840029a2b0b3bd2bd5fc0209ca
u6k <u6k.apps@gmail.com>
2019-01-22 22:48:59 +0900

    WIP: Writting

--- lib/crawline.rb
+++ lib/crawline.rb
@@ -14,13 +14,10 @@ module Crawline
         http.request(req)
       end
 
-      sleep(1)
-
       if res.code == "200"
         res.body
       else
-        Rails.logger.warn "NetModule#download_with_get: status code not 200 ok: url=#{url}, code=#{res.code}"
-        nil
+        raise "#{res.code} #{res.message}"
       end
     end
   end
@@ -37,44 +34,46 @@ module Crawline
     end
 
     def self.put_s3_object(bucket, file_name, data)
+      # FIXME: Remove
       # data compress
-      data_7z = StringIO.new("")
-      SevenZipRuby::Writer.open(data_7z) do |szr|
-        szr.level = 9
-        szr.add_data(data, file_name.split("/")[-1])
-      end
+      #data_7z = StringIO.new("")
+      #SevenZipRuby::Writer.open(data_7z) do |szr|
+      #  szr.level = 9
+      #  szr.add_data(data, file_name.split("/")[-1])
+      #end
 
-      data_7z.rewind
-      raise "Compress error" if not SevenZipRuby::Reader.verify(data_7z)
+      #data_7z.rewind
+      #raise "Compress error" if not SevenZipRuby::Reader.verify(data_7z)
 
-      data_7z.rewind
-      data_7z = data_7z.read
+      #data_7z.rewind
+      #data_7z = data_7z.read
 
       # upload
-      obj_original = bucket.object(file_name + ".7z")
-      obj_original.put(body: data_7z)
+      obj_original = bucket.object(file_name)
+      obj_original.put(body: data)
 
-      obj_backup = bucket.object(file_name + ".7z.bak_" + DateTime.now.strftime("%Y%m%d-%H%M%S"))
-      obj_backup.put(body: data_7z)
+      obj_backup = bucket.object(file_name + ".bak_" + DateTime.now.strftime("%Y%m%d-%H%M%S"))
+      obj_backup.put(body: data)
 
       { original: obj_original.key, backup: obj_backup.key }
     end
 
     def self.get_s3_object(bucket, file_name)
       # download
-      object = bucket.object(file_name + ".7z")
-      data_7z = object.get.body.read(object.size)
+      object = bucket.object(file_name)
+      data = object.get.body.read(object.size)
 
+      # FIXME: Remove
       # data extract
-      data_7z = StringIO.new(data_7z)
-      raise "Extract error" if not SevenZipRuby::Reader.verify(data_7z)
+      #data_7z = StringIO.new(data_7z)
+      #raise "Extract error" if not SevenZipRuby::Reader.verify(data_7z)
 
-      data_7z.rewind
+      #data_7z.rewind
 
-      data = nil
-      SevenZipRuby::Reader.open(data_7z) do |szr|
-        data = szr.extract_data(file_name.split("/")[-1])
-      end
+      #data = nil
+      #SevenZipRuby::Reader.open(data_7z) do |szr|
+      #  data = szr.extract_data(file_name.split("/")[-1])
+      #end
 
       data
     end
--- spec/crawline_spec.rb
+++ spec/crawline_spec.rb
@@ -1,35 +1,106 @@
 require "spec_helper"
+require "webmock/rspec"
+require "aws-sdk-s3"
 
-describe Crawline do
+describe "Crawline" do
   it "has a version number" do
     expect(Crawline::VERSION).not_to be nil
   end
 
-  describe Downloader do
+  describe "Downloader" do
+    before do
+      WebMock.enable!
+
+      WebMock.stub_request(:get, "http://test.crawline.u6k.me/200.html").to_return(
+        body: "Response 200 OK",
+        status: [200, "OK"],
+        headers: {
+          "Content-Type" => "text/plain"
+        })
+
+      WebMock.stub_request(:get, "http://test.crawline.u6k.me/301.html").to_return(
+        body: "Response 301 Moved Permanently",
+        status: [301, "Moved Permanently"],
+        headers: {
+          "Location" => "http://test.crawline.u6k.me/200.html"
+        })
+
+      WebMock.stub_request(:get, "http://test.crawline.u6k.me/404.html").to_return(
+        body: "Response 404 Not Found",
+        status: [404, "Not Found"])
+
+      WebMock.stub_request(:get, "http://test.crawline.u6k.me/500.html").to_return(
+        body: "Response 500 Internal Server Error",
+        status: [500, "Internal Server Error"])
+    end
+
+    after do
+      WebMock.disable!
+    end
+
     it "download successful" do
-      expect(false).to eq(true)
+      download_result = Crawline::Downloader.download_with_get("http://test.crawline.u6k.me/200.html")
+
+      expect(download_result).to eq("Response 200 OK")
     end
 
     it "download with redirect" do
-      expect(false).to eq(true)
-    end
+      download_result = Crawline::Downloader.download_with_get("http://test.crawline.u6k.me/301.html")
 
-    it "download success with 3xx" do
-      expect(false).to eq(true)
+      expect(download_result).to eq("Response 200 OK")
     end
 
     it "download fail with 4xx" do
-      expect(false).to eq(true)
+      expect {
+        Crawline::Downloader.download_with_get("http://test.crawline.u6k.me/404.html")
+      }.to raise_error(RuntimeError, "404 Not Found")
     end
 
     it "download fail with 5xx" do
-      expect(false).to eq(true)
+      expect {
+        Crawline::Downloader.download_with_get("http://test.crawline.u6k.me/500.html")
+      }.to raise_error(RuntimeError, "500 Internal Server Error")
+    end
+
+    it "download fail with timeout" do
+      expect {
+        Crawline::Downloader.download_with_get("http://test.crawline.u6k.me/timeout")
+      }.to raise_error(RuntimeError, "Timeout")
     end
   end
 
-  describe ResourceRepository do
+  describe "ResourceRepository" do
     it "put web page data" do
-      expect(false).to eq(true)
+      bucket = get_s3_bucket
+
+      Crawline::ResourceRepository.put_s3_object(bucket, "put_test.txt", "put test")
+
+      obj = bucket.object("put_test.txt")
+      expect(obj.get.body.read(obj.size)).to eq("put test")
+    end
+
+    it "get web page data" do
+      bucket = get_s3_bucket
+
+      obj = bucket.object("get_test.txt")
+      obj.put(body: "get test")
+
+      data = Crawline::ResourceRepository.get_s3_object(bucket, "get_test.txt")
+
+      expect(data).to eq("get test")
+    end
+
+    def get_s3_bucket
+      Aws.config.update({
+        region: ENV["AWS_S3_REGION"],
+        credentials: Aws::Credentials.new(ENV["AWS_S3_ACCESS_KEY"], ENV["AWS_S3_SECRET_KEY"])
+      })
+      s3 = Aws::S3::Resource.new(endpoint: ENV["AWS_S3_ENDPOINT"], force_path_style: ENV["AWS_S3_FORCE_PATH_STYLE"])
+
+      bucket = s3.bucket(ENV["AWS_S3_BUCKET"])
+      bucket.create if not bucket.exists?
+
+      bucket
     end
   end
 end
