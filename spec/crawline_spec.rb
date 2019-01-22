0db5602eca9aa685489c8337ed14258a5de20031
u6k <u6k.apps@gmail.com>
2019-01-22 15:15:22 +0900

    WIP: Writting test

--- spec/crawline_spec.rb
+++ spec/crawline_spec.rb
@@ -5,7 +5,31 @@ describe Crawline do
     expect(Crawline::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe Downloader do
+    it "download successful" do
+      expect(false).to eq(true)
+    end
+
+    it "download with redirect" do
+      expect(false).to eq(true)
+    end
+
+    it "download success with 3xx" do
+      expect(false).to eq(true)
+    end
+
+    it "download fail with 4xx" do
+      expect(false).to eq(true)
+    end
+
+    it "download fail with 5xx" do
+      expect(false).to eq(true)
+    end
+  end
+
+  describe ResourceRepository do
+    it "put web page data" do
+      expect(false).to eq(true)
+    end
   end
 end
